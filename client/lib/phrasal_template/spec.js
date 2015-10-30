dbcb4a61b52878abf5764ae5d6e5b91165a3abc1
Andrew Shaw Care <andrewshawcare@gmail.com>
2015-10-29 23:12:11 -0500

    Phrasal template is a function

--- client/lib/phrasal_template/spec.js
+++ client/lib/phrasal_template/spec.js
@@ -1,7 +1,15 @@
 define(["./index"], function (PhrasalTemplate) {
+
   describe("Phrasal template", function () {
+
     it("is defined", function () {
       expect(PhrasalTemplate).toBeDefined();
     });
+
+    it("is a function", function () {
+      expect(typeof PhrasalTemplate).toBe("function");
+    });
+    
   });
+
 });
