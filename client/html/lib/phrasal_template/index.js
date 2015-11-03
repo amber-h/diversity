44978058e4e878155bc517a156ea357e8b018f3c
Amber Houle <amber.houle3@gmail.com>
2015-11-02 23:38:00 -0500

    Prepending jquery phrasal template variables with $

--- client/html/lib/phrasal_template/index.js
+++ client/html/lib/phrasal_template/index.js
@@ -1,41 +1,41 @@
 define([], function () {
   return function (phrase) {
-    var element = $("<div>").addClass("phrasal-template");
+    var $element = $("<div>").addClass("phrasal-template");
 
     if (typeof phrase === "string") {
-      element.text(phrase);
+      $element.text(phrase);
     }
 
     if (Array.isArray(phrase)) {
       phrase.forEach(function (phraseComponent) {
-        var phraseComponentElement;
+        var $phraseComponentElement;
 
         switch (phraseComponent.type) {
           case "collocation":
-            phraseComponentElement = $("<span>").addClass("collocation").text(phraseComponent.value);
+            $phraseComponentElement = $("<span>").addClass("collocation").text(phraseComponent.value);
           break;
           case "slot":
-            phraseComponentElement = $("<select>").addClass("slot");
-            phraseComponentElement.attr("data-name", phraseComponent.value.name);
-            phraseComponentElement.on("change", function () {
-              phraseComponent.value.onchange({name: phraseComponent.value.name, value: phraseComponentElement.val()});
+            $phraseComponentElement = $("<select>").addClass("slot");
+            $phraseComponentElement.attr("data-name", phraseComponent.value.name);
+            $phraseComponentElement.on("change", function () {
+              phraseComponent.value.onchange({name: phraseComponent.value.name, value: $phraseComponentElement.val()});
             });
 
             phraseComponent.value.options.forEach(function (option) {
-              var optionElement = $("<option>");
-              optionElement.attr("value", option)
-              optionElement.attr("label", option)
+              var $optionElement = $("<option>");
+              $optionElement.attr("value", option)
+              $optionElement.attr("label", option)
 
-              phraseComponentElement.append(optionElement);
+              $phraseComponentElement.append($optionElement);
             });
           break;
           default:
           break;
         }
-        element.append(phraseComponentElement);
+        $element.append($phraseComponentElement);
       });
     }
 
-    return element[0];
+    return $element[0];
   };
 });
