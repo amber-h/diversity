96dbb034dbd3bd1b9dec77c72c3c301615c4faa9
Amber Houle <amber.houle3@gmail.com>
2015-12-02 21:17:05 -0500

    Adding stats endpoint to server to eventually retrieve jigsaw saw

--- server/server.rb
+++ server/server.rb
@@ -1,13 +1,26 @@
 require 'json'
 require 'sinatra'
 require 'rethinkdb'
-require 'envyable'
 include RethinkDB::Shortcuts
-Envyable.load('./config/env.yml', "#{ENV['RACK_ENV']}")
 
-get '/people' do
-  connection = r.connect(:host => 'rethinkdb', :port => 28015)
+class Server < Sinatra::Base
+  set :bind, '0.0.0.0'
+  set :port, 4567
 
-  headers 'Access-Control-Allow-Origin' => '*'
-  body r.db(ENV['DB_NAME']).table('people').run(connection).entries.to_json
+  get '/people' do
+    connection = r.connect(:host => 'rethinkdb', :port => 28015)
+
+    headers 'Access-Control-Allow-Origin' => '*'
+    body r.db(ENV['DB_NAME']).table('people').run(connection).entries.to_json
+  end
+
+  post '/stats' do
+    headers 'Access-Control-Allow-Origin' => '*'
+
+    queryOptions = JSON.parse request.body.read
+    puts queryOptions
+
+    response
+  end
+
 end
