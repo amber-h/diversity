ed07c0cd33bc562c233f4cd5e987ea04fb69cdd3
Amber Houle <amber.houle3@gmail.com>
2015-12-02 22:54:39 -0500

    Sending request from client to server with jigsaw queryOptions

--- client/html/lib/query_service/index.js
+++ client/html/lib/query_service/index.js
@@ -1,5 +1,14 @@
-define([], function () {
+define([], function() {
   return function(queryOptions) {
+
+    var sendRequest = function(request) {
+      $.post("http://192.168.99.100:4567/stats", JSON.stringify(request),
+        function(data, status) {
+          alert('success:' + data)
+        });
+    };
+    sendRequest(queryOptions);
+
     var results = {};
     return results;
   };
