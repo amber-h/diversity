f344afad6c6a16ab9f1a574e24aec82ad4e14b44
Amber Houle <amber.houle3@gmail.com>
2015-12-04 07:43:42 -0500

    Small Refactoring: Pulling out common db connection into instance variable

--- server/lib/thoughtworker_repository.rb
+++ server/lib/thoughtworker_repository.rb
@@ -3,16 +3,18 @@ include RethinkDB::Shortcuts
 
 class ThoughtWorkerRepository
 
+  def initialize
+    @connection = r.connect(:host => 'rethinkdb', :port => 28015)
+    @db = r.db('dev')
+  end
+
   def all
-    connection = r.connect(:host => 'rethinkdb', :port => 28015)
-    r.db('dev').table('people').run(connection).entries
+    @db.table('people').run(@connection).entries
   end
 
   def find_by_role_and_location role, location
-    connection = r.connect(:host => 'rethinkdb', :port => 28015)
-    people = r.db('dev').table('people')
-
-    people.filter(:role => {:name => role}).run(connection).entries
+    people = @db.table('people')
+    people.filter(:role => {:name => role}).run(@connection).entries
   end
 
 
